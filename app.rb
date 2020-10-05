9513bd6a30f87d90c0ffb6c99f5f7f8847c4b9ea
thesebonesalone <duikhead2@gmail.com>
2020-10-04 20:32:38 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Will"
+    end
+    get "/hometown" do
+        "My hometown is Colorado Springs"
+    end
+    get "/favorite-song" do
+        "My favorite song is an undefined entity, it changes on a daily basis, please don't make me choose"
+    end
+    
+
+
+
 end
